0892225d3dad0883efb85f06edbaf6935eb116aa
Daniela Foggiatto <danielafoggiatto2014@hotmail.com>
2025-05-16 14:04:28 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double volume, raio = 2.5, altura = 3.2;
+
+        volume = 3.14159 * raio * raio * altura;
+        System.out.println("A altura é = " + altura);
+        System.out.println("O raio é = " + raio);
+        System.out.printf("O VOLUME É = %.3f\n", volume);
+    }
+    
+}
